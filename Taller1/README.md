5dccfae7cba0fa2b962fd002d885579b36d80832
Antonio Josué Gaitan Quesada <agaitanq513@ulacit.ed.cr>
2018-10-15 18:42:31 -0600

    Taller1
    inicio

---
+++ Taller1/README.md
@@ -0,0 +1,2 @@
+# Taller #1 - Programación Orientada a Objetos
+Manejo de Publicaciones y Comentarios
---
+++ Taller1/Taller1/Publicacion.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taller1
+{
+    class Publicacion
+    {
+    }
+}
