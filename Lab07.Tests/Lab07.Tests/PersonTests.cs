01c64081ccb10abf23835b8f84cda51cf5d81ce9
Antonio Josué Gaitán Quesada <agaitanq513@ulacit.ed.cr>
2018-10-29 19:08:09 -0600

    Primer Commit
    Creación Solución

---
+++ Lab07.Tests/Lab07.Tests/PersonTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab07.Tests
+{
+    [TestClass]
+    public class PersonTests
+    {
+        [TestMethod]
+        public void APersonIsCreaterd()
+        {
+            //Arrange: configura todo lo que va a utilizar
+
+            //Act: llama al método
+
+            //Assert: que tuvo que pasar
+        }
+
+
+        [TestMethod]
+        public void APersonMustHaveAName()
+        {
+
+        }
+
+        [TestMethod]
+        public void TheAgeOfAPersonIsCalculatedCorrectly()
+        {
+
+        }
+
+    }
+}
---
+++ Lab07.Tests/Lab07.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab07.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab07.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("573e9e79-3fc9-459f-83f5-e53f89c2e15f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
